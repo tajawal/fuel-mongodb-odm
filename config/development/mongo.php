ff44b81c986c30e58f43fb10ca032370d818deeb
Ian Berry <iberry@barracuda.com>
2013-11-04 18:14:14 -0500

    break out mongo config file for development and production

---
+++ config/development/mongo.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+	'email' => array(
+		'hostname'  => '10.26.135.253',
+		'port'      => 27017,
+		'database'  => 'email',
+		'username'  => '',
+		'password'  => '',
+		'options'   => array(),
+		'profiling' => FALSE
+	),
+	'config' => array(
+		'hostname'  => '10.26.135.253',
+		'port'      => 27017,
+		'database'  => 'config',
+		'username'  => '',
+		'password'  => '',
+		'options'   => array(),
+		'profiling' => FALSE
+	)
+
+);
---
+++ config/production/mongo.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+	'email' => array(
+		'hostname'  => '127.0.0.1',
+		'port'      => 27017,
+		'database'  => 'email',
+		'username'  => '',
+		'password'  => '',
+		'options'   => array(),
+		'profiling' => FALSE
+	),
+	'config' => array(
+		'hostname'  => '127.0.0.1',
+		'port'      => 27017,
+		'database'  => 'config',
+		'username'  => '',
+		'password'  => '',
+		'options'   => array(),
+		'profiling' => FALSE
+	)
+
+);
