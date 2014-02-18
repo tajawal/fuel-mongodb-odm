88774bb320404b92218c4ca5c1bbf4708f7ad3e6
Ian Berry <ian@lift.me>
2014-02-18 13:05:05 -0500

    remove reference to 10.26.135.253

--- config/development/mongo.php
+++ config/development/mongo.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'email' => array(
-		'hostname'  => '10.26.135.253',
+		'hostname'  => '127.0.0.1',
 		'port'      => 27017,
 		'database'  => 'email',
 		'username'  => '',
@@ -10,7 +10,7 @@ return array(
 		'profiling' => TRUE
 	),
 	'config' => array(
-		'hostname'  => '10.26.135.253',
+		'hostname'  => '127.0.0.1',
 		'port'      => 27017,
 		'database'  => 'config',
 		'username'  => '',
